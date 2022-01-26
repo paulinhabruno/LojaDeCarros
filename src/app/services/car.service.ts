9261e2b6eef42044001c6be099344fa112c7096c
paulinhabruno <mapalurj@gmail.com>
2022-01-26 12:02:03 -0300

    requisição e manipulação de erros

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
+import { Car } from '../models/car';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,41 @@ export class CarService {
   constructor(private httpClient: HttpClient) { }
 
   httpOptions = {
-
     headers: new HttpHeaders({'Content-Type': 'application/json'})
 
   }
+
+//obter todos os carros
+getCars(): Observable<Car[]>{
+  return this.httpClient.get<Car[]>(this.url)
+  .pipe(
+  retry(2),
+  catchError(this.handleError)
+  )
+}
+//Obter o carro pelo Id
+getCarsById(id: number): Observable<Car>{
+  return this.httpClient.get<Car>(this.url + '/' + id)
+  .pipe(
+  retry(2),
+  catchError(this.handleError)
+  )
+}
+
+// Manipulação de erros
+
+handleError(error: HttpErrorResponse) {
+  let errorMessage = 'Não foi possivel fazer a solicitação :(';
+  if (error.error instanceof ErrorEvent) {
+    // Erro ocorreu no lado do client
+    errorMessage = error.error.message;
+  } else {
+    // Erro ocorreu no lado do servidor
+    errorMessage =
+      `Código do erro: ${error.status}, ` + `mensagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
 }
